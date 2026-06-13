1cca6af517e5d1b65aafd3440dc01ef207c87b5b
karl487 <ksmith35@byupathway.edu>
2026-06-14 00:31:54 +0300

    commit

---
+++ week05/scripts/form.js
@@ -0,0 +1,5 @@
+const currentYearElement = document.getElementById('currentyear');
+currentYearElement.textContent = new Date().getFullYear();
+
+const lastModifiedElement = document.getElementById('lastmodified');
+lastModifiedElement.textContent =  document.lastModified;
